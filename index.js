c9b88809b6ab899aea5cd07fc0e4e7838774a9b2
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-25 15:33:16 +0300

    3.3: puhelinluettelon backend step3 tehty.

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find(person => person.id === id);
+
+  if (!person) {
+    return res.status(404).end();
+  }
+
+  res.json(person);
+})
+
 const port = 3001;
 app.listen(port);
 
