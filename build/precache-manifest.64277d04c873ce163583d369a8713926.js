42e2202829df691b8bcb9cab93cb22a3837fae93
Panu Valtanen <panu.valtanen@pinja.com>
2020-08-28 11:31:33 +0300

    3.11: puhelinluettelo full stack tehty

---
+++ build/precache-manifest.64277d04c873ce163583d369a8713926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749811e076efa63e0d29f0ce1542866f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb92580eeab2e92f15b",
+    "url": "/static/css/main.f09dfeea.chunk.css"
+  },
+  {
+    "revision": "16ee3d411164fc5738fc",
+    "url": "/static/js/2.e657b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e657b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb92580eeab2e92f15b",
+    "url": "/static/js/main.325fbcf7.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
