64fa2d6e3607ac29977ef50fc9690c191355d68c
Panu Valtanen <panu.valtanen@pinja.com>
2020-08-28 11:05:23 +0300

    3.10: puhelinluettelon backend step10 preparation of heroku

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port);
 
 console.log(`Server running on port ${port}`);
