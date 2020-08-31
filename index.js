f825fbf5c93991dca82b38ff2d38faaadc883b64
Panu Valtanen <p4nu@users.noreply.github.com>
2020-08-31 17:34:52 +0300

    3.13: puhelinluettelo ja tietokanta, step1 tehty.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 const app = express();
 
 app.use(express.json());
@@ -13,29 +14,6 @@ morgan.token('body', req => {
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-let persons = [
-  {
-    id: 0,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 1,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 2,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 3,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/info', (req, res) => {
   res.send(`
@@ -81,7 +59,12 @@ app.post('/api/persons', (req, res) => {
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person
+    .find({})
+    .then(people => {
+      res.json(people);
+    })
+    .catch(error => console.error('Cannot find Person document from database:',error.message));
 });
 
 app.get('/api/persons/:id', (req, res) => {
