9b1acc33dbebb7f5e67c7bbe0929b2f397384277
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-02 19:44:40 +0300

    3.17: puhelinluettelo ja tietokanta step5 tehty.

--- index.js
+++ index.js
@@ -82,10 +82,27 @@ app.get('/api/persons/:id', (req, res) => {
   }
 
   res.json(person);
-})
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person
+    .findByIdAndUpdate(req.params.id, person, {
+      new: true,
+    })
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(error => next(error));
+});
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
+  Person
+    .findByIdAndRemove(req.params.id)
     .then(() => {
       res.status(204).end();
     })
